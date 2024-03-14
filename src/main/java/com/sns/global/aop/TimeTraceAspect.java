9ccbd22d6e27183edfddbdcca3262999fa918550
wooseok <wooseok6008@gmail.com>
2024-03-15 03:47:39 +0900

    feat(timeTraceAspect): 실행 속도 측정 aop

---
+++ src/main/java/com/sns/global/aop/TimeTraceAspect.java
@@ -0,0 +1,26 @@
+package com.sns.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class TimeTraceAspect {
+
+    @Around("execution(* com.sns.domain..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            log.info("Method: {} Execution Time: {}ms", joinPoint.getSignature().toShortString(),
+                timeMs);
+        }
+    }
+}
